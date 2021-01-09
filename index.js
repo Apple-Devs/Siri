7a8baad39cad084084bf7d1f4414004ee3c36909
Captain-TNT <66196010+Teghjyot@users.noreply.github.com>
2021-01-10 03:21:47 +1100

    Canvas Feature Testing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const { translate } = require("google-translate-api-browser");
+const Canvas = require('canvas');
 const fs = require("fs");
 const langs = {"auto":"Automatic","af":"Afrikaans","sq":"Albanian","am":"Amharic","ar":"Arabic","hy":"Armenian","az":"Azerbaijani","eu":"Basque","be":"Belarusian","bn":"Bengali","bs":"Bosnian","bg":"Bulgarian","ca":"Catalan","ceb":"Cebuano","ny":"Chichewa","zh-cn":"Chinese Simplified","zh-tw":"Chinese Traditional","co":"Corsican","hr":"Croatian","cs":"Czech","da":"Danish","nl":"Dutch","en":"English","eo":"Esperanto","et":"Estonian","tl":"Filipino","fi":"Finnish","fr":"French","fy":"Frisian","gl":"Galician","ka":"Georgian","de":"German","el":"Greek","gu":"Gujarati","ht":"Haitian Creole","ha":"Hausa","haw":"Hawaiian","iw":"Hebrew","hi":"Hindi","hmn":"Hmong","hu":"Hungarian","is":"Icelandic","ig":"Igbo","id":"Indonesian","ga":"Irish","it":"Italian","ja":"Japanese","jw":"Javanese","kn":"Kannada","kk":"Kazakh","km":"Khmer","ko":"Korean","ku":"Kurdish (Kurmanji)","ky":"Kyrgyz","lo":"Lao","la":"Latin","lv":"Latvian","lt":"Lithuanian","lb":"Luxembourgish","mk":"Macedonian","mg":"Malagasy","ms":"Malay","ml":"Malayalam","mt":"Maltese","mi":"Maori","mr":"Marathi","mn":"Mongolian","my":"Myanmar (Burmese)","ne":"Nepali","no":"Norwegian","ps":"Pashto","fa":"Persian","pl":"Polish","pt":"Portuguese","pa":"Punjabi","ro":"Romanian","ru":"Russian","sm":"Samoan","gd":"Scots Gaelic","sr":"Serbian","st":"Sesotho","sn":"Shona","sd":"Sindhi","si":"Sinhala","sk":"Slovak","sl":"Slovenian","so":"Somali","es":"Spanish","su":"Sundanese","sw":"Swahili","sv":"Swedish","tg":"Tajik","ta":"Tamil","te":"Telugu","th":"Thai","tr":"Turkish","uk":"Ukrainian","ur":"Urdu","uz":"Uzbek","vi":"Vietnamese","cy":"Welsh","xh":"Xhosa","yi":"Yiddish","yo":"Yoruba","zu":"Zulu"}
 const langsInverted = {};
@@ -34,6 +35,9 @@ client.on("message", (message) => {
   let revArgs = message.content.split(/ +/).reverse();
   let operation, object, searchEngine;
   let b;
+  if (args.includes("bruh")) {
+    message.channel.send("https://tenor.com/view/bruh-moment-bruh-moment-recording-gif-14698316");
+  }
 
   const checkHeySiriJs = require ( "./commands/heySiri.js"            ) ;
   const profanityJs    = require ( "./commands/checkForSwearWords.js" ) ;
@@ -327,6 +331,42 @@ client.on("message", (message) => {
     translateIt();
   }
 
+  toile = () => {
+    var canvasWidth = 1000;
+    var canvasHeight = 1000;
+    
+    const canvas = Canvas.createCanvas(canvasWidth, canvasHeight);
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = "#030f33"
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    drawStar = (x, y, w) => {
+      var rad = ctx.createRadialGradient(x, y, w/1.5, x, y, w*2);
+        rad.addColorStop(0, "white");
+        rad.addColorStop(1, "#030f33");
+      ctx.beginPath();
+      ctx.arc(x, y, w*2, 0, 2 * Math.PI);
+      ctx.fillStyle = rad;
+      ctx.fill();
+      ctx.closePath();
+    }
+
+    for (var i = 0; i < Math.floor(Math.random() * 250) + 100; i++) {
+      var starX = Math.floor(Math.random() * canvasWidth) + 1
+      var starY = Math.floor(Math.random() * canvasHeight) + 1
+      var starW = Math.floor(Math.random() * 7) + 1;
+
+      drawStar(starX, starY, starW)
+
+    }
+    
+
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer());
+    message.channel.send(attachment);   
+
+  }
+
   operation = checkOperation();
   checkForSwearWords()
 
@@ -392,6 +432,10 @@ client.on("message", (message) => {
 
       case "translate":
         traduisez();
+        break;
+      
+      case "toire":
+        toile();
         break;
     }
   }
